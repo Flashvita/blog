5a4d386272b81bd0fafe297d7da2dbcc7ab7d4eb
Vitaliy Kuleshov <vitalijkulesov975@gmail.com>
2022-03-31 13:24:34 +0300

    conrol commit

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    intro = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(70), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 
 @app.route('/')
-@app.route('/lk')
 def base():
     return render_template('base.html')
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
 
-@app.route('/lk/<string:name>/<int:id>')
-def user(name, id):
-    return "Personal lk: " + name + " :" + str(id) + "old"
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        content = request.form['content']
+        article = Article(title=title, intro=intro, content=content)
 
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error create article"
+    else:
+        return render_template('create-article.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
